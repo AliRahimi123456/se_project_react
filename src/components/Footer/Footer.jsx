a0834ed545a4cc4fa207e38dfb31edf4d8330c00
AliRahimi <ali.1996rahimiourzgani@gmail.com>
2025-02-05 13:16:01 -0500

    fixed the errors

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,12 @@
-import "./Footer.css"
-function Footer() {
-    return (
-        <div className="footer">
-            
-                <p>Developed by Ali</p>
-                <p>2025</p>
-            
-        </div>
+import "./Footer.css";
 
-    )
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Developed by Ali</p>
+      <p>2025</p>
+    </footer>
+  );
 }
+
 export default Footer;
